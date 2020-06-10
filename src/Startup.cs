b5ad572eb25e8b16b447f2b19053d7c2e216a37f
Zak Herman <zakanator@gmail.com>
2020-06-10 13:51:41 +1200

    Refactor submissions controller to moderation controller.

--- src/Startup.cs
+++ src/Startup.cs
@@ -22,7 +22,7 @@ namespace Submissions.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<IQueueStorageRepository, QueueStorageRepository>();
+            services.AddScoped<IModerationQueueStorageRepository, ModerationQueueStorageRepository>();
             services.AddControllers();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         }
