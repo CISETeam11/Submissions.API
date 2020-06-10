ff27d5ffbea6a0aea91694750fa3476c24a30fcd
Zak Herman <zakanator@gmail.com>
2020-06-10 18:41:19 +1200

    Add model validation.

---
+++ src/Filters/RangeUntilCurrentYearAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Submissions.API.Filters
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    public class RangeUntilCurrentYearAttribute : RangeAttribute
+    {
+        public RangeUntilCurrentYearAttribute(int minimum) : base(minimum, DateTime.UtcNow.Year)
+        {
+        }
+    }
+}
--- src/Models/Article.cs
+++ src/Models/Article.cs
@@ -1,24 +1,32 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Submissions.API.Filters;
 
 namespace Submissions.API.Models
 {
     public class Article
     {
+        [StringLength(1024)]
         public string Author { get; set; }
 
+        [StringLength(1024)]
         public string Title { get; set; }
 
+        [StringLength(255)]
         public string Journal { get; set; }
 
-        public int Year { get; set; }
+        [RangeUntilCurrentYear(1800)]
+        public int? Year { get; set; }
 
         public int? JournalIssue { get; set; }
 
         public int? Volume { get; set; }
 
+        [StringLength(12)]
         public string Pages { get; set; }
 
         [Required]
+        [StringLength(255)]
+        [RegularExpression(@"^10\.\d{4,9}\/[-.;()\/:\w]+$", ErrorMessage = "The field Doi does not match the DOI specification format.")]
         public string Doi { get; set; }
     }
 }
